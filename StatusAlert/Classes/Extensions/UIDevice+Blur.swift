8399cfcd0e236d4bb4442059397c36e26675b9c9
Yegor Miroshnichenko <mierosh@gmail.com>
2017-12-31 16:15:13 +0200

    Added blur availability check utils

---
+++ StatusAlert/Classes/Extensions/UIDevice+Blur.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+extension UIDevice {
+    
+    /// If the device is able to display blur:
+    ///
+    /// - if operation system supports blur;
+    /// - if "Reduce transparency" mode is disabled;
+    /// - if device supports blur.
+    var isBlurAvailable: Bool {
+        guard operationSystemSupportsBlur,
+            !reduceTransparencyEnabled,
+            deviceSupportsBlur
+            else {
+                return false
+        }
+        
+        return true
+    }
+    
+    private var operationSystemSupportsBlur: Bool {
+        if #available(iOS 8.0, *) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    private var reduceTransparencyEnabled: Bool {
+        return UIAccessibilityIsReduceTransparencyEnabled()
+    }
+    
+    private var deviceSupportsBlur: Bool {
+        return platform.isNewerThan(UIDevice.DevicePlatform.iPadModel.iPad3)
+            || platform.isNewerThan(UIDevice.DevicePlatform.iPodTouchModel.iPodTouch4)
+            || platform.isNewerThan(UIDevice.DevicePlatform.iPhoneModel.iPhone4)
+    }
+}
